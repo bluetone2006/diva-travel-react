d90f6b0940dc1338a7025ec793217a14ce23852e
nixmin23 <86486020+nixmin23@users.noreply.github.com>
2023-03-11 16:02:18 -0500

    Create index.js

---
+++ components/LoadingPage/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { PulpdisplayBoldWhite28px } from "../../styledMixins";
+import "./LoadingPage.css";
+function LoadingPage(props) {
+  const {
+    children
+  } = props;
+  return /*#__PURE__*/React.createElement("div", {
+    className: "container-center-horizontal"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "loading-page screen"
+  }, /*#__PURE__*/React.createElement(OverlapGroup, null, /*#__PURE__*/React.createElement(Link, {
+    to: "/destination-page"
+  }, /*#__PURE__*/React.createElement(Rectangle13, null)), /*#__PURE__*/React.createElement(Loading, null, children))));
+}
+const OverlapGroup = styled.div`
+  width: 430px;
+  height: 932px;
+  position: relative;
+`;
+const Rectangle13 = styled.div`
+  position: absolute;
+  width: 430px;
+  height: 932px;
+  top: 0;
+  left: 0;
+  background-color: var(--sea-serpent);
+  cursor: pointer;
+`;
+const Loading = styled.h1`
+  ${PulpdisplayBoldWhite28px}
+  position: absolute;
+  top: 452px;
+  left: 154px;
+  letter-spacing: 0;
+  line-height: 28px;
+  white-space: nowrap;
+`;
+export default LoadingPage;
